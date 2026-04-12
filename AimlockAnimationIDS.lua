8a93ec5ec51fb062f8c8ec3ced45462a94493544
bytenetowner <bytenetowner@gmail.com>
2026-04-12 18:37:18 +0300

    Refactor AssetIds and remove GetAssetIds function
    Refactor AssetIds to return table directly and remove unused function.

--- AimlockAnimationIDS.lua
+++ AimlockAnimationIDS.lua
@@ -1,13 +1,6 @@
-local AssetIds = {
+return {
 	"rbxassetid://70869035406359",
 	"rbxassetid://102810363618918",
 	"rbxassetid://106673226682917",
 	"rbxassetid://120428956410756"
 }
-
-local function GetAssetIds()
-	for _, AssetId in ipairs(AssetIds) do
-		print(AssetId)
-	end
-	return AssetIds
-end
